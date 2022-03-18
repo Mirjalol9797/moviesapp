748ff6dae8eaf84f13d1da44f4c097af5f9d3b5d
Mirxomitov Mirjalol <miramirxomitov97@gmail.com>
2022-03-18 23:11:16 +0500

    added api for axios, create axios modules

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="home">Home page</div>
+  <div class="home">
+    Home page
+    {{ text }}
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "Home",
+  data() {
+    return {
+      text: "Text",
+    };
+  },
   components: {},
+  mounted() {
+    this.fetchMovies();
+  },
+  methods: {
+    ...mapActions("moviesStore", ["fetchMovies"]),
+  },
 };
 </script>
